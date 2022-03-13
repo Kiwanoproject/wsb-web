a73cb11bc1edcb849023fdd89d54d5ef916a8fd3
Kiwanoproject <kiwanoproject@gmail.com>
2022-03-13 18:28:11 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { ethers } from "ethers";
 import dayjs from "dayjs";
 import { useLottie } from "lottie-react";
 import BigNumber from "bignumber.js";
+import Web3Modal from "web3modal";
+import WalletConnect from "@walletconnect/web3-provider";
 import ClaimingAnimation from "./assets/ClaimingAnimation.json";
 import Whitelist from "./assets/whitelist-signed.json";
 import { aWSBAirDropABI } from "./ABI/airdrop.json";
@@ -24,6 +26,8 @@ import success from "./assets/success-mini.png";
 import binanceLogo from "./assets/binance-logo.png";
 import metamask from "./assets/metamask.png";
 import "./App.less";
+import { useWeb3React } from "@web3-react/core";
+import { resolve } from "dns";
 
 declare global {
   interface Window {
@@ -31,46 +35,58 @@ declare global {
   }
 }
 
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnect,
+    infuraId: "00ca1859789d4b40bce01f4104844224",
+    options: {
+      rpc: {
+        56: "https://bsc-dataseed1.binance.org",
+      },
+      network: "binance",
+      chainId: 56,
+    },
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions, // required
+});
+
 const ethereum = window.ethereum;
+
 let aWSBAirDropContract: ethers.Contract;
 let aWSBTokenContract: ethers.Contract;
-
 let FairyAirDropContract: ethers.Contract;
 
-//const aWSBTokenAddress = AWSB_TOKEN_ADDRESS;
-//const aWSBAirDropContractAddress = AWSB_AIRDROP_CONTRACT_ADDRESS;
-//const FairyAirDropContractAddress = FAIRY_CONTRACT_ADDRESS;
+const aWSBTokenAddress = AWSB_TOKEN_ADDRESS;
+const aWSBAirDropContractAddress = AWSB_AIRDROP_CONTRACT_ADDRESS;
+const FairyAirDropContractAddress = FAIRY_CONTRACT_ADDRESS;
 
 //DEV:
-const aWSBTokenAddress = AWSB_TOKEN_ADDRESS_TEST;
-const aWSBAirDropContractAddress = AWSB_AIRDROP_CONTRACT_ADDRESS_TEST;
-const FairyAirDropContractAddress = FAIRY_CONTRACT_ADDRESS_TEST;
+// const aWSBTokenAddress = AWSB_TOKEN_ADDRESS_TEST;
+// const aWSBAirDropContractAddress = AWSB_AIRDROP_CONTRACT_ADDRESS_TEST;
+// const FairyAirDropContractAddress = FAIRY_CONTRACT_ADDRESS_TEST;
 
-let ethersProvider: any;
-let signer: any;
-if (ethereum) {
-  ethersProvider = new ethers.providers.Web3Provider(ethereum, "any");
-  signer = ethersProvider.getSigner();
-}
-console.log("🚀 ~ file: App.tsx ~ line 33 ~ ethersProvider", ethersProvider);
+// let ethersProvider: any;
+// let signer: any;
+// let chainId: any;
+// if (ethereum) {
+// }
 
-const init = async () => {
-  aWSBAirDropContract = new ethers.Contract(
-    aWSBAirDropContractAddress,
-    aWSBAirDropABI,
-    ethersProvider
-  );
-  FairyAirDropContract = new ethers.Contract(
-    FairyAirDropContractAddress,
-    Fairy,
-    ethersProvider
-  );
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  aWSBTokenContract = new ethers.Contract(aWSBTokenAddress, EIP20);
-  aWSBTokenContract = aWSBTokenContract.connect(signer);
-  aWSBAirDropContract = aWSBAirDropContract.connect(signer);
-  FairyAirDropContract = FairyAirDropContract.connect(signer);
-};
+// async function main() {
+//   const instance = await web3Modal.connect();
+//   ethersProvider = new ethers.providers.Web3Provider(instance);
+//   signer = ethersProvider.getSigner();
+//   chainId = (await ethersProvider.getNetWork()).chainId;
+//   console.log("chainId", chainId);
+// }
+
+// main();
+
+// console.log("🚀 ~ file: App.tsx ~ line 33 ~ ethersProvider", ethersProvider);
 
 const formatAddress = (address: string) => {
   return address.substr(0, 8) + "..." + address.substr(address.length - 8, 8);
@@ -132,6 +148,9 @@ async function addToMask() {
 }
 
 function App() {
+  const [ethersProvider, setEthersProvider] =
+    useState<ethers.providers.Web3Provider>();
+  const [signer, setSigner] = useState<ethers.Signer>();
   const [address, setAddress] = useState<string>("");
   const [expiredTime, setExpiredTime] = useState<number>(0);
   const [claimBalance, setClaimBalance] = useState<string>("0");
@@ -140,49 +159,130 @@ function App() {
   const [isMetaMaskConnected, setIsMetaMaskConnected] = useState<boolean>();
   const [claiming, setClaiming] = useState<boolean>(false);
   const [claimSuccess, setClaimSuccess] = useState<boolean>(false);
-  const [connecting, setConnecting] = useState<boolean>(true);
+  const [connecting, setConnecting] = useState<boolean>(false);
   const [noWallet, setNotWallet] = useState<boolean>(false);
   const [isFairy, setIsFairy] = useState<boolean>(false);
   const [isFairyEvent, setIsFairyEvent] = useState<boolean>(false);
   const [fairyNextReleasedTime, setFairyNextReleasedTime] = useState<number>(0);
   const [fairyClaimBalance, setFairyClaimBalance] = useState<string>("0");
 
-  useEffect(() => {
-    if (!ethereum) {
-      setNotWallet(true);
-      setConnecting(false);
-      return;
-    }
-    let chainId: number;
-    setConnecting(true);
-    const getNetWork = async () => {
-      chainId = (await ethersProvider.getNetwork()).chainId;
-      // dev: BSC_TESTNET_ID
-	  if (chainId !== BSC_TESTNET_ID) {
+  // useEffect(() => {
+  //   if (!ethereum) {
+  //     setNotWallet(true);
+  //     setConnecting(false);
+  //     return;
+  //   }
+  //   if (ethersProvider !== undefined) {
+  //     let chainId: number;
+  //     setConnecting(true);
+  //     const getNetWork = async () => {
+  //       chainId = (await ethersProvider.getNetwork()).chainId;
+  //       // dev: BSC_MAINNET_ID
+  //       if (chainId !== BSC_MAINNET_ID) {
+  //         setErrorNetWork(true);
+  //         setAddress("");
+  //       }
+  //     };
+  //     getNetWork();
+  //     const getAccount = async () => {
+  //       await new Promise((resolve) => setTimeout(resolve, 1000));
+  //       setIsMetaMaskConnected(Boolean(ethereum.selectedAddress));
+  //       setConnecting(false);
+  //     };
+  //     getAccount();
+  //   }
+  // }, []);
+
+  const connectToWallet = async () => {
+    try {
+      setConnecting(true);
+      const instance = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(instance);
+      setEthersProvider(provider);
+      const sign = provider.getSigner();
+      setSigner(sign);
+      const networkId = await provider.getNetwork();
+      console.log(networkId);
+      const addr = await sign.getAddress();
+      console.log("Address", addr);
+      setAddress(addr);
+      if (networkId.chainId !== BSC_MAINNET_ID) {
         setErrorNetWork(true);
         setAddress("");
       }
-    };
-    getNetWork();
-    async function getAccount() {
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      setIsMetaMaskConnected(Boolean(ethereum.selectedAddress));
+      setIsMetaMaskConnected(Boolean(address));
+      setConnecting(false);
+
+      // Subscribe to accounts change
+      instance.on("accountsChanged", (accounts: string[]) => {
+        console.log(accounts);
+        setAddress(accounts[0]);
+      });
+
+      // Subscribe to chainId change
+      instance.on("chainChanged", (chainId: number) => {
+        console.log(chainId);
+        if (chainId !== BSC_MAINNET_ID) {
+          setErrorNetWork(true);
+          setAddress("");
+        }
+      });
+
+      // Subscribe to provider connection
+      instance.on("connect", (info: { chainId: number }) => {
+        console.log(info);
+      });
+
+      // Subscribe to provider disconnection
+      instance.on("disconnect", (error: { code: number; message: string }) => {
+        setIsMetaMaskConnected(false);
+        setAddress("");
+        setConnecting(false);
+        console.log(error);
+      });
+    } catch (error) {
       setConnecting(false);
     }
-    getAccount();
-  }, []);
+  };
 
-  const change = (accounts: any[]) => {
-    setIsMetaMaskConnected(
-      accounts.length > 0 && Boolean(ethereum.selectedAddress)
-    );
-    if (!(accounts.length > 0 && Boolean(ethereum.selectedAddress))) {
-      setAddress("");
+ /* useEffect(() => {
+    if (!isMetaMaskConnected) {
+      connectToWallet();
+    }
+  }, []); */
+
+  // const change = (accounts: any[]) => {
+  //   setIsMetaMaskConnected(
+  //     accounts.length > 0 && Boolean(ethereum.selectedAddress)
+  //   );
+  //   if (!(accounts.length > 0 && Boolean(ethereum.selectedAddress))) {
+  //     setAddress("");
+  //   }
+  // };
+  // if (ethereum) {
+  //   ethereum.on("accountsChanged", change);
+  // }
+
+  const init = async () => {
+    if (signer) {
+      aWSBAirDropContract = new ethers.Contract(
+        aWSBAirDropContractAddress,
+        aWSBAirDropABI,
+        ethersProvider
+      );
+      FairyAirDropContract = new ethers.Contract(
+        FairyAirDropContractAddress,
+        Fairy,
+        ethersProvider
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      aWSBTokenContract = new ethers.Contract(aWSBTokenAddress, EIP20);
+      aWSBTokenContract = aWSBTokenContract.connect(signer);
+      aWSBAirDropContract = aWSBAirDropContract.connect(signer);
+      FairyAirDropContract = FairyAirDropContract.connect(signer);
     }
   };
-  if (ethereum) {
-    ethereum.on("accountsChanged", change);
-  }
 
   const getAirdropInfos = async () => {
     await init();
@@ -191,11 +291,10 @@ function App() {
         method: "eth_requestAccounts",
       });
       setAddress(walletAccounts[0]);
-      let aWSBTokenBalance: ethers.BigNumber = await aWSBTokenContract.balanceOf(
-        walletAccounts[0]
-      );
+      let aWSBTokenBalance: ethers.BigNumber =
+        await aWSBTokenContract.balanceOf(walletAccounts[0]);
 
-      let expiredTime = ethers.BigNumber.from("1621310400");
+      let expiredTime = ethers.BigNumber.from("1721310400");
       let claimBalance = ethers.BigNumber.from("0");
 
       let account = walletAccounts[0];
@@ -203,15 +302,17 @@ function App() {
       //console.log(Whitelist);
       console.log(account.toLowerCase());
       if (claimInfo) {
-        let claimed = await aWSBAirDropContract.hashBlacklist(claimInfo["hash"]);
+        let claimed = await aWSBAirDropContract.hashBlacklist(
+          claimInfo["hash"]
+        );
         expiredTime = ethers.BigNumber.from(claimInfo["expiredAt"]);
-        if (claimed == false) {
+        if (claimed === false) {
           claimBalance = ethers.BigNumber.from(claimInfo["amount"]);
         }
       }
-      
+
       let fairyStatus = false;
-      
+
       /*
       let expiredTime: ethers.BigNumber = await aWSBAirDropContract.claimExpiredAt();
       let claimBalance: ethers.BigNumber = await aWSBAirDropContract.claimWhitelist(
@@ -221,20 +322,17 @@ function App() {
         walletAccounts[0]
       );*/
       if (fairyStatus) {
-        let nextReleasedTime: ethers.BigNumber = await FairyAirDropContract.nextReleasedTime();
+        let nextReleasedTime: ethers.BigNumber =
+          await FairyAirDropContract.nextReleasedTime();
         console.log(
           "🚀 ~ file: App.tsx ~ line 202 ~ getAirdropInfos ~ nextReleasedTime",
           nextReleasedTime.toNumber()
         );
-        let fairyBalance: ethers.BigNumber = await FairyAirDropContract.fairyVault(
-          walletAccounts[0]
-        );
+        let fairyBalance: ethers.BigNumber =
+          await FairyAirDropContract.fairyVault(walletAccounts[0]);
         setFairyNextReleasedTime(nextReleasedTime.toNumber());
         setFairyClaimBalance(
-          new BigNumber(fairyBalance.toString())
-            .div(1e18)
-            .toFixed(4)
-            .toString()
+          new BigNumber(fairyBalance.toString()).div(1e18).toFixed(4).toString()
         );
       }
       setIsFairy(fairyStatus);
@@ -250,10 +348,7 @@ function App() {
           .toString()
       );
       setClaimBalance(
-        new BigNumber(claimBalance.toString())
-          .div(1e18)
-          .toFixed(4)
-          .toString()
+        new BigNumber(claimBalance.toString()).div(1e18).toFixed(4).toString()
       );
     }
   };
@@ -285,7 +380,13 @@ function App() {
         process = await FairyAirDropContract.claim();
       } else {
         let claimInfo = Whitelist[address.toLowerCase()];
-        process = await aWSBAirDropContract.claim(claimInfo["amount"], claimInfo["expiredAt"], claimInfo["v"], claimInfo["r"], claimInfo["s"]);
+        process = await aWSBAirDropContract.claim(
+          claimInfo["amount"],
+          claimInfo["expiredAt"],
+          claimInfo["v"],
+          claimInfo["r"],
+          claimInfo["s"]
+        );
       }
       try {
         await process.wait();
@@ -447,7 +548,7 @@ function App() {
               style={{
                 background: "#4B2CC8",
               }}
-              onClick={connectWallet}
+              onClick={connectToWallet}
               disabled={connecting}
             >
               {connecting ? "Connecting..." : "Unlock Wallet"}
